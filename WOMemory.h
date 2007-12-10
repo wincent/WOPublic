a1ddd30b573cebf8c7e78c786c044c13f1a3116f
Wincent Colaiuta <win@wincent.com>
2007-12-10 12:06:11 +0100

    Add WOMemory.h
    Start with a simple inline function, wmalloc, which encapsulates the "try
    to malloc memory and throw an exception on failure" pattern.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ WOMemory.h
@@ -0,0 +1,26 @@
+//
+// WOMemory.h
+// WOPublic
+//
+// Created by Wincent Colaiuta on 10 December 2007.
+// Copyright 2007 Wincent Colaiuta.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+static inline void *wmalloc(size_t size)
+{
+    void *buffer = malloc(size);
+    if (!buffer)
+        [NSException raise:NSMallocException format:@"malloc of %d bytes failed", size];
+    return buffer;
+}
