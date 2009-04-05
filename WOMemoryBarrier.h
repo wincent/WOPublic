2acbd24bc8942cb2e7ba4b2141e4eee5a096fb28
Wincent Colaiuta <win@wincent.com>
2009-04-05 11:59:38 +0200

    Import WOMemoryBarrier.h from WOCommon
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ WOMemoryBarrier.h
@@ -0,0 +1,152 @@
+//
+//  WOMemoryBarrier.h
+//  WOCommon
+//
+//  Created by Wincent Colaiuta on 30 November 2004.
+//  Copyright 2004-2008 Wincent Colaiuta.
+
+//! \file
+//! Memory barrier macros for ppc, ppc64, i386/SSE and i386
+//!
+//! This file provides a full bi-directional memory barrier macro, WO_MEMORY_BARRIER, and the following uni-directional barriers:
+//!
+//!     - the WO_WRITE_MEMORY_BARRIER; also known as a "store" or "release" memory barrier, or an "upwards fence": all stores before the barrier are guaranteed to take place before any stores after the barrier.
+//!     - the WO_READ_MEMORY_BARRIER; also known as a "load" or "acquire" memory barrier, or a "downwards fence": all loads before the barrier are guaranteed to take place before any loads after the barrier; a read barrier is also an implied "data dependency" barrier, guaranteeing the correct ordering and visibility of two loads where the second load depends on the result of the first.
+//!
+//! Memory barriers should always come in pairs. If they are correctly placed they prevent operations performed on one CPU from being seen out-of-order by another CPU, something that would otherwise be possible due to the potentionally aggressive instruction re-ordering performed by modern compilers and CPUs, and the possibility of incoherency between per-processor caches.
+//!
+//! The following code illustrates correct placement of memory barriers in a singleton implementation that uses the Double-Checked Locking pattern:
+//!
+//! \code
+//! id instance = shared_instance;              // 1
+//! WO_READ_MEMORY_BARRIER();                   // 2
+//! if (instance == nil)
+//! {
+//!     @synchronized (object)
+//!     {
+//!         instance = shared_instance;
+//!         if (instance == nil)
+//!         {
+//!             instance = alloc_instance();    // 3
+//!             WO_WRITE_MEMORY_BARRIER();      // 4
+//!             shared_instance = instance;     // 5
+//!         }
+//!     }
+//! }
+//! return instance;
+//! \endcode
+//!
+//! The principal purpose of the memory barriers in the above example is to ensure that:
+//!
+//!     - only one instance of the singleton is allocated (by the alloc_instance() function)
+//!     - the allocation is fully completed before any other threads are allowed to see the shared instance
+//!
+//! To imagine the effects of the pair of barriers one must imagine two threads, each on a different processor, trying to execute the code simultaneously. The @synchronized block already guarantees that only one thread can enter the critical synchronzied section at a time, but without the memory barriers there is no guarantee that another thread won't see the operations <em>inside</em> the critical section as happening in a different order.
+//!
+//! This is because even a seemingly atomic operation like the following:
+//!
+//! \code
+//! shared_instance = alloc_instance();
+//! \encode
+//!
+//! Is in actuality a two-step process:
+//!
+//! \code
+//! // allocate an instance
+//! instance = alloc_instance();
+//!
+//! // make "shared_instance" point at allocated instance
+//! shared_instance = instance;
+//! \encode
+//!
+//! On a single-processor machine with a single cache there are no problems with this non-atomicity because the CPU will always see these events as being in the correct order. On a multi-processor machine with multiple caches, however, the order of the events may be perceived differently from different CPUs. This is because when the changes are propagated from one cache to another they may not necessarily be propagated in the same order; the shared_instance pointer may be updated first, for example, <em>before</em> the memory indicated by the pointer (the actual instance) itself is updated.
+//!
+//! The call to WO_WRITE_MEMORY_BARRIER at "4" prevents any stores from moving past it in either direction; that is, the allocation ("3") must complete before the assignment to the shared_instance variable ("5").
+//!
+//! The call to WO_READ_MEMORY_BARRIER at "2" is necessary because without it the ordering enforced by the corresponding write barrier would not necessarily be visible from another processor. That is, the read of shared_instance at "1", in conjunction with the read memory barrier at "2", causes all effects prior to the <em>storage</em> of shared_instance (at "5") to be visible; in this case the "effect" that we are interested in is the completion of the alloc_instance() function at "3".
+//!
+//! Simplifying to the extreme, the write memory barrier effectively alters the state of shared memory, while read memory barrier ensures that the reader sees the new state. Only when used in pairs can these memory barriers offer useful guarantees.
+//!
+//! \sa http://kerneltrap.org/node/6431
+//! \sa http://www.nwcpp.org/Downloads/2004/DCLP_notes.pdf
+
+#if defined(__ppc__)
+
+// ppc
+
+//! Prevents loads being reordered across the point at which it appears.
+//! It implies a data dependency barrier.
+//! It is typically used immediately after a critical read (load) operation, and used in conjunction with a corresponding write memory barrier.
+#define WO_READ_MEMORY_BARRIER()    __asm__ __volatile__ ("sync":::"memory")
+
+//! Prevents stores being reordered across the point at which it appears.
+//! It is typically used immediately before a critical write (store) operation, and used in conjunction with a corresponding read memory barrier.
+#define WO_WRITE_MEMORY_BARRIER()   __asm__ __volatile__ ("eieio":::"memory")
+
+//! Prevents loads and stores being reordered across the point at which it appears.
+//! This is a full bi-directional "fence" that combines the functionality of both a read and a write memory barrier.
+#define WO_MEMORY_BARRIER()         __asm__ __volatile__ ("sync":::"memory")
+
+#elif defined(__i386__)
+#if defined(__SSE__)
+
+// i386 with SSE
+#define WO_READ_MEMORY_BARRIER()    __asm__ __volatile__ ("lfence":::"memory")
+#define WO_WRITE_MEMORY_BARRIER()   /* no-op: not needed on i386 */
+#define WO_MEMORY_BARRIER()         __asm__ __volatile__ ("mfence":::"memory")
+
+#else /* not defined __SSE__ */
+
+// i386 without SSE
+#define WO_READ_MEMORY_BARRIER()    __asm__ __volatile__ ("lock; addl $0,0(%%esp)":::"memory")
+#define WO_WRITE_MEMORY_BARRIER()   /* no-op: not needed on i386 */
+#define WO_MEMORY_BARRIER()         __asm__ __volatile__ ("lock; addl $0,0(%%esp)":::"memory")
+
+#endif
+#elif defined(__ppc64__)
+
+// ppc64
+#define WO_READ_MEMORY_BARRIER()    __asm__ __volatile__ ("lwsync":::"memory")
+#define WO_WRITE_MEMORY_BARRIER()   __asm__ __volatile__ ("sync":::"memory")
+#define WO_MEMORY_BARRIER()         __asm__ __volatile__ ("sync":::"memory")
+
+#else
+
+#warning Unsupported architecture
+
+// fall back to OS-level, bi-directional fences
+#define WO_READ_MEMORY_BARRIER()    OSMemoryBarrier()
+#define WO_WRITE_MEMORY_BARRIER()   OSMemoryBarrier()
+#define WO_MEMORY_BARRIER()         OSMemoryBarrier()
+
+#endif
+
+//! Usage:
+//!
+//! \code
+//! id myGlobalVariable = nil;
+//! + (id)mySingleton
+//! {
+//!     // use double-checked locking pattern with memory barriers
+//!     // to safely initialize and return myGlobalVariable using
+//!     // [[self alloc] init]:
+//!     WO_DCLP_INIT(myGlobalVariable, [[self alloc] init]);
+//! }
+//! \endcode
+#define WO_RETURN_DCLP_INIT(sharedVar, initializer) \
+    id instance = sharedVar;                        \
+    WO_READ_MEMORY_BARRIER();                       \
+    if (!instance)                                  \
+    {                                               \
+        @synchronized (self)                        \
+        {                                           \
+            instance = sharedVar;                   \
+            if (!instance)                          \
+            {                                       \
+                instance = (initializer);           \
+                WO_WRITE_MEMORY_BARRIER();          \
+                sharedVar = instance;               \
+            }                                       \
+        }                                           \
+    }                                               \
+    return instance;
