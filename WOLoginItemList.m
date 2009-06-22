32f13acff57ff33f7cb4178f900c4f3a9bcc77af
Wincent Colaiuta <win@wincent.com>
2009-06-22 19:07:26 +0200

    Clean up WOLoginItem and WOLoginItem for consistency with WOPublic
    Update headers (update copyright ranges, remove unnecessary lines, add
    license notice) and do some minor re-wrapping for better display on
    narrow terminals.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- WOLoginItemList.m
+++ WOLoginItemList.m
@@ -1,19 +1,38 @@
+// WOLoginItemList.m
+// WOPublic
 //
-//  WOLoginItemList.m
-//  WOCommon
+// Copyright 2007-2009 Wincent Colaiuta. All rights reserved.
 //
-//  Created by Wincent Colaiuta on 9 December 2007.
-//  Copyright 2007-2008 Wincent Colaiuta.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 // class header
 #import "WOLoginItemList.h"
 
+// macro headers
+#import "WODebugMacros.h"
+#import "WOMemory.h"
+
 // other class headers
 #import "WOLoginItem.h"
-
-// WOPublic headers
-#import "WOPublic/WODebugMacros.h"
-#import "WOPublic/WOMemory.h"
 
 @interface WOLoginItemList ()
 
@@ -158,9 +177,13 @@
 #pragma mark -
 #pragma mark NSFastEnumeration protocol
 
-- (NSUInteger)countByEnumeratingWithState:(NSFastEnumerationState *)state objects:(id *)stackbuf count:(NSUInteger)len
+- (NSUInteger)countByEnumeratingWithState:(NSFastEnumerationState *)state
+                                  objects:(id *)stackbuf
+                                    count:(NSUInteger)len
 {
-    return [self.items countByEnumeratingWithState:state objects:stackbuf count:len];
+    return [self.items countByEnumeratingWithState:state
+                                           objects:stackbuf
+                                             count:len];
 }
 
 #pragma mark -
