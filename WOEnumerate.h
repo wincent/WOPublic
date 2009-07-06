3f02c032e1007d3bd300cbfcd9d9ba4556c53af0
Wincent Colaiuta <win@wincent.com>
2009-07-06 13:13:52 +0200

    Rewrap Doxygen comments in WOEnumerate.h
    For consistency with the other headers in the repo, wrap code comments
    to 80 columns.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- WOEnumerate.h
+++ WOEnumerate.h
@@ -24,31 +24,45 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-/*! WO_ENUMERATE was a convenience macro for expressing the Objective-C enumerator idiom in more compact form. Instead of the standard, longer form:
-
-<pre>NSEnumerator *enumerator = [collection objectEnumerator];
-id object = nil;
-while ((object = [enumerator nextObject]))
-    NSLog(@"Object: %@", object);</pre>
-
-The following, shorter form was used:
-
-<pre>WO_ENUMERATE(collection, object)
-    NSLog(@"Object: %@", object);</pre>
-
-The WO_ENUMERATE macro was also considerably faster than the standard form because is used a cached IMP (implementation pointer) and selector to speed up repeated invocations of the <tt>nextObject</tt> selector. In informal testing (enumerating over a 10,000,000-item array ten times) the macro performed 49% faster than the standard idiom (averaging 3.6 million objects per second compared with 2.4 million per second).
-
-If passed a nil pointer instead of a valid collection, no iterations were performed. If passed an object which did not respond to the objectEnumerator selector then an exception was raised. Both of these behaviours matched the pattern of the standard idiom.
-
-Note that the compiler C dialect must be set to C99 or GNU99 in order to use this macro because of the initialization of variables inside the for expression.
-
-When Apple announced Objective-C 2.0 (for Leopard), which includes a "for" enumeration construct in the language itself, the WO_ENUMERATE macro was marked as deprecated and finally removed. The WO_REVERSE_ENUMERATE and WO_KEY_ENUMERATE are retained because they have no language-level equivalent.
-
-\sa http://mjtsai.com/blog/2003/12/08/cocoa_enumeration/
-\sa http://rentzsch.com/papers/improvingCocoaObjCEnumeration/
-\sa http://mjtsai.com/blog/2006/07/15/cocoa-foreach-macro/
-
-*/
+//! WO_ENUMERATE was a convenience macro for expressing the Objective-C
+//! enumerator idiom in more compact form. Instead of the standard, longer
+//! form:
+//!
+//! <pre>NSEnumerator *enumerator = [collection objectEnumerator];
+//! id object = nil;
+//! while ((object = [enumerator nextObject]))
+//!     NSLog(@"Object: %@", object);</pre>
+//!
+//! The following, shorter form was used:
+//!
+//! <pre>WO_ENUMERATE(collection, object)
+//!     NSLog(@"Object: %@", object);</pre>
+//!
+//! The WO_ENUMERATE macro was also considerably faster than the standard form
+//! because is used a cached IMP (implementation pointer) and selector to speed
+//! up repeated invocations of the <tt>nextObject</tt> selector. In informal
+//! testing (enumerating over a 10,000,000-item array ten times) the macro
+//! performed 49% faster than the standard idiom (averaging 3.6 million objects
+//! per second compared with 2.4 million per second).
+//!
+//! If passed a nil pointer instead of a valid collection, no iterations were
+//! performed. If passed an object which did not respond to the
+//! objectEnumerator selector then an exception was raised. Both of these
+//! behaviours matched the pattern of the standard idiom.
+//!
+//! Note that the compiler C dialect must be set to C99 or GNU99 in order to
+//! use this macro because of the initialization of variables inside the for
+//! expression.
+//!
+//! When Apple announced Objective-C 2.0 (for Leopard), which includes a "for"
+//! enumeration construct in the language itself, the WO_ENUMERATE macro was
+//! marked as deprecated and finally removed. The WO_REVERSE_ENUMERATE and
+//! WO_KEY_ENUMERATE are retained because they have no language-level
+//! equivalent.
+//!
+//! \sa http://mjtsai.com/blog/2003/12/08/cocoa_enumeration/
+//! \sa http://rentzsch.com/papers/improvingCocoaObjCEnumeration/
+//! \sa http://mjtsai.com/blog/2006/07/15/cocoa-foreach-macro/
 
 #ifndef WO_REVERSE_ENUMERATE
 
